df949a48ea59532df02f01675c6e41eabdc731d5
Alejandro <aromanarguello@gmail.com>
2019-11-24 22:16:19 -0500

    second test for button

--- components/Buttons/Button.test.js
+++ components/Buttons/Button.test.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { renderWithTheme } from "../../utils/tests/utils";
+import { fireEvent } from "@testing-library/react-native";
 import Button from "./Button";
 
 test("accepts title", () => {
   const { getByTestId, ...rest } = renderWithTheme(<Button title="click me" />);
-  console.log(rest);
   const buttonNode = getByTestId("button");
-  console.log(buttonNode.child);
   expect(buttonNode).toBeTruthy();
+});
+
+test("can click", () => {
+  const mockFn = jest.fn();
+  const { getByTestId, ...rest } = renderWithTheme(
+    <Button onSubmitHandler={mockFn} title="click me" />
+  );
+  const buttonNode = getByTestId("button");
+  fireEvent.press(buttonNode);
+  expect(mockFn).toHaveBeenCalledTimes(1);
 });
